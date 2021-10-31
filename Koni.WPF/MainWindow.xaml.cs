2a22ad40e422fdb6814728246a85d9778ca5c249
Tianto <64172441+sibernaut@users.noreply.github.com>
2021-10-31 23:44:24 +0700

    Add license files.

--- Koni.WPF/MainWindow.xaml.cs
+++ Koni.WPF/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Koni.Engine.Wrapper;
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using Koni.Engine.Wrapper;
 using System.IO.Abstractions;
 using System.Windows;
 using System.Windows.Input;
